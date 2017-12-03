e1b262134d657794a8079c423a9625c6511e88a6
Gregory Siems <gsiems@gmail.com>
2017-12-03 17:18:40 -0600

    Additional documentation

--- pkg/isbn/isnb.go
+++ pkg/isbn/isnb.go
@@ -1,4 +1,11 @@
 // Package isbn validates, parses, and converts International Standard Book Number (ISBN) data.
+//
+// In addition to simply checking the length, characters and check-digit
+// of an ISBN this also parses the ISBN using the ISBN ranges data
+// available from https://www.isbn-international.org/range_file_generation
+// in order to verify that the Prefix, Registration Group, and Registrant
+// elements of the ISBN are valid.
+
 package isbn
 
 import (
